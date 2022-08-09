5aefdc180e2bb9f1b51a653637049fbd950af9bf
sparklingwaterlemon <michaelkim009@gmail.com>
2022-08-09 16:02:06 -0700

    issues here

--- JS/script222.js
+++ JS/script222.js
@@ -1,5 +1,5 @@
-
 function drag(event, value) {
+    console.log("dragging...");
     event.dataTransfer.setData("number", value);
     event.dataTransfer.setData("text", event.target.id);
 }
@@ -11,9 +11,31 @@ function allowDrop(event) {
 function drop(event) {
     event.preventDefault();
 
+    var chipValue = Number(event.dataTransfer.getData("number"));
+    console.log(typeof chipValue);
+    console.log(chipValue);
+
+
     var data = event.dataTransfer.getData("text");
-    event.target.appendChild(document.getElementById(data));
+    // event.target.appendChild(document.getElementById(data));
+    event.target.appendChild(document.getElementById(data).cloneNode(true));
+    console.log(data);
+    console.log(event.target);
+
+}
+
+
+function remove(event, value) {
+    event.preventDefault();
 
     var chipValue = Number(event.dataTransfer.getData("number"));
-    console.log(chipValue);
+    
+
+    var data = event.dataTransfer.getData("text");
+    console.log(data);
+    
+    var betChip = document.getElementById("data");
+    console.log(betChip);
+    board.removeChild(betChip);
+    
 }
